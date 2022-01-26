119f1ca53adb2090948ac508332db56d4d1d55b1
Taha Khan <muhammad.taha@hysabkytab.com>
2022-01-26 21:01:21 +0500

    index js file updated

--- index.js
+++ index.js
@@ -196,6 +196,73 @@ db.sequelize
         console.error("❗️ Could not connect to redis database...", err);
         process.exit();
       });
+    /**
+     * Normalize a port into a number, string, or false.
+     */
+    function normalizePort(val) {
+      var port = parseInt(val, 10);
+      if (isNaN(port)) {
+        return val;
+      }
+      if (port >= 0) {
+        return port;
+      }
+      return false;
+    }
+
+    /**
+     * Event listener for HTTP server "error" event.
+     */
+    function terminate(server, options = { coredump: false, timeout: 500 }) {
+      // Exit function
+      const exit = (code) => {
+        options.coredump ? process.abort() : process.exit(code);
+      };
+
+      return (code, reason) => (err, promise) => {
+        if (err && err instanceof Error) {
+          // Log error information, use a proper logging library here :)
+          fs.appendFileSync("access.log", err.message);
+          console.log(err.message, err.stack);
+        }
+
+        // Attempt a graceful shutdown
+        // server.close(exit);
+        // setTimeout(exit, options.timeout).unref();
+      };
+    }
+
+    function exitHandler(options, exitCode) {
+      terminate(server, {
+        coredump: false,
+        timeout: 500,
+      });
+      console.log("⚠️ Gracefully shutting down");
+      server.close();
+      process.exit();
+    }
+
+    process.on("uncaughtException", (err) => {
+      fs.appendFile(
+        "access.log",
+        `Uncaught Exception: ${err.message}`,
+        () => {}
+      );
+      console.log(`Uncaught Exception: ${err.message}`);
+    });
+    process.on("unhandledRejection", (reason, promise) => {
+      fs.appendFile(
+        "access.log",
+        `Unhandled rejection, reason: ${reason}`,
+        () => {}
+      );
+      console.log("Unhandled rejection at", promise, `reason: ${reason}`);
+    });
+    process.on("SIGINT", exitHandler.bind(null, { exit: true }));
+
+    // catches "kill pid" (for example: nodemon restart)
+    process.on("SIGUSR1", exitHandler.bind(null, { exit: true }));
+    process.on("SIGUSR2", exitHandler.bind(null, { exit: true }));
   })
   .catch((err) => {
     console.error(`❌ Server Stopped (listening on PORT : ${PORT})`);
