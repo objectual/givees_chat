55df5681653939fd9d4820f9f51448f3a0bfb8b3
Taha Khan <muhammad.taha@hysabkytab.com>
2022-01-26 19:53:59 +0500

    docker file added

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,13 @@
 # Specifies where to get the base image (Node v12 in our case) and creates a new container for it
-FROM node:12.18.1
+FROM node
 
-# Set working directory. Paths will be relative this WORKDIR.
-WORKDIR ./root
+WORKDIR /.
 
 # Install dependencies
-COPY ["package.json", "package-lock.json*", "./"]
+ADD package*.json ./
+# Copy source files from host computer to the container
+ADD . .
 RUN npm install
 
-# Copy source files from host computer to the container
-COPY . .
-
-# Specify port app runs on
-EXPOSE 3000
-
 # Run the app
-# CMD ["./cloud_sql_proxy", "-instances=givees-295311:asia-south1:givees-dev-db1=tcp:3306", "-credential_file=./credentials.json", "&", "&&", "sleep","10", "&&", "npm", "start"]
-CMD ["npm", "start"]
+CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var moment = require("moment");
 const cors = require("cors");
 const http = require("http");
 const path = require("path");
-const socketioJwt   = require('socketio-jwt');
+const socketioJwt = require("socketio-jwt");
 //const io = require('socket.io')(http);
 const app = express();
 
@@ -18,8 +18,8 @@ const { connect_cache } = require("./cache/redis.service");
 const handle = require("./Middleware/error");
 const swaggerUi = require("swagger-ui-express"),
   swaggerDocument = require("./swagger.json");
-const { default_settings } = require("./user_default_settings");
-const PORT = process.env.PORT || 5000;
+// const { default_settings } = require("./user_default_settings");
+const PORT = process.env.PORT;
 const server = http.createServer(app);
 
 app.use(cors());
@@ -33,114 +33,127 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-
-
-
-const socketio = require('socket.io');
+const socketio = require("socket.io");
 const io = socketio(server);
 const fs = require("fs");
 var publicKEY = fs.readFileSync("config/cert/public.key", "utf8");
-const { addUser, removeUser, getUser, getUsersInRoom, addChat, getChat, ChatFriendlist, UpdateReadMessages, LeaveRoom, SearchFriends } = require('./SocketIo/users');
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+  addChat,
+  getChat,
+  ChatFriendlist,
+  UpdateReadMessages,
+  LeaveRoom,
+  SearchFriends,
+} = require("./SocketIo/users");
 
+io.use(
+  socketioJwt.authorize({
+    secret: publicKEY,
+    handshake: true,
+  })
+);
+io.on("connection", async (socket) => {
+  socket.on("userlist", async (pageno, pagesize, callback) => {
+    const { FriendListArr } = await ChatFriendlist(
+      socket.decoded_token.id,
+      pageno,
+      pagesize
+    );
 
+    socket.emit("users", FriendListArr);
 
-
-
-
-io.use(socketioJwt.authorize({
-      secret: publicKEY,
-      handshake: true
-    }));
- io.on('connection', async (socket) => {
-  
-  socket.on('userlist', async (pageno, pagesize ,callback) => {
-  
-  const { FriendListArr } = await ChatFriendlist(socket.decoded_token.id, pageno, pagesize);
-  
-  socket.emit("users", FriendListArr);
-   
     callback();
   });
-  
 
-  socket.on('Searchuserlist', async (pageno , pagesize , name ,callback) => {
-    
-    const { FriendsArr } = await SearchFriends(socket.decoded_token.id, name, pageno, pagesize);
-    
+  socket.on("Searchuserlist", async (pageno, pagesize, name, callback) => {
+    const { FriendsArr } = await SearchFriends(
+      socket.decoded_token.id,
+      name,
+      pageno,
+      pagesize
+    );
+
     socket.emit("Searchusers", FriendsArr);
-     
-      callback();
-    });
-  
-   
-  socket.on('join', async (data, callback) => {
-    
 
-    
-    const { error, user } = await addUser({ id: socket.decoded_token.id, data });
-    
-    
-    if(error) return callback(error);
-    
+    callback();
+  });
+
+  socket.on("join", async (data, callback) => {
+    const { error, user } = await addUser({
+      id: socket.decoded_token.id,
+      data,
+    });
+
+    if (error) return callback(error);
+
     socket.join(user.roomid);
 
-    
     //Send Old messages in DB
     const { history } = await getChat(user.roomid);
-    socket.emit('message', history[0]);
-    
+    socket.emit("message", history[0]);
+
     //All message Read
     UpdateReadMessages(socket.decoded_token.id);
-    
-    socket.broadcast.to(user.roomid).emit('message',  {});
 
-    io.to(user.roomid).emit('roomData', { room: user.roomid, users: getUsersInRoom(user.roomid) });
-    
+    socket.broadcast.to(user.roomid).emit("message", {});
+
+    io.to(user.roomid).emit("roomData", {
+      room: user.roomid,
+      users: getUsersInRoom(user.roomid),
+    });
+
     callback();
   });
 
-  
+  socket.on("sendMessage", async (roomid, message, callback) => {
+    const user = await getUser(socket.decoded_token.id, roomid);
 
-  socket.on('sendMessage', async (roomid, message,callback) => {
-   
-  const user = await getUser(socket.decoded_token.id, roomid);
-  
-  if(!user){
-    let error = "This user is not connected";
-    return callback(error);
-  }  
-  const useravailable = await getUser(user.data, roomid);
-  let read = 0; 
-   if(!useravailable){
-     read = 1;
-   }
+    if (!user) {
+      let error = "This user is not connected";
+      return callback(error);
+    }
+    const useravailable = await getUser(user.data, roomid);
+    let read = 0;
+    if (!useravailable) {
+      read = 1;
+    }
     let time = new Date();
-    io.to(user.roomid).emit('message', { SenderId: socket.decoded_token.id, ReceiverId: user.data,Message: message, createdAt: time});
-   
-    addChat({senderid:socket.decoded_token.id, receiverid:user.data, roomid:user.roomid, message, Isread: read});
-    
-   
+    io.to(user.roomid).emit("message", {
+      SenderId: socket.decoded_token.id,
+      ReceiverId: user.data,
+      Message: message,
+      createdAt: time,
+    });
+
+    addChat({
+      senderid: socket.decoded_token.id,
+      receiverid: user.data,
+      roomid: user.roomid,
+      message,
+      Isread: read,
+    });
+
     callback();
   });
 
-  
-  socket.on('leave', async (roomid, callback) => {
-    const user = LeaveRoom(socket.decoded_token.id,roomid);
-    
-  callback();
+  socket.on("leave", async (roomid, callback) => {
+    const user = LeaveRoom(socket.decoded_token.id, roomid);
+
+    callback();
   });
 
-  socket.on('disconnect', () => {
-    
+  socket.on("disconnect", () => {
     const user = removeUser(socket.decoded_token.id);
-    
-    
+
     // if(user) {
     //   io.to(user.roomid).emit('message', { user: 'Admin', text: `${user.name} has left.` });
     //   io.to(user.roomid).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
     // }
-  })
-
+  });
 });
 
 const db = require("./Model");
@@ -165,16 +178,16 @@ db.sequelize
             console.info(`✔️ (${process.env.NODE_ENV}) ENV Loaded`);
           }
           console.info(`⌚`, moment().format("DD-MM-YYYY hh:mm:ss a"));
-          default_settings()
-            .then(() => {
-              console.log(`✔️ Default Data Set`);
-            })
-            .catch((e) => {
-              if (e) {
-                console.error("❗️ Could not execute properly", e);
-              }
-              console.log(`✔️ Default Data Exists`);
-            });
+          // default_settings()
+          //   .then(() => {
+          //     console.log(`✔️ Default Data Set`);
+          //   })
+          //   .catch((e) => {
+          //     if (e) {
+          //       console.error("❗️ Could not execute properly", e);
+          //     }
+          //     console.log(`✔️ Default Data Exists`);
+          //   });
         });
       })
       .catch((err) => {
@@ -193,12 +206,10 @@ db.sequelize
 
 // check for expiry voucher and campaign at every 6th hour
 
-
 app.use("/api", require("./Startup/api"));
 app.use("/cache", require("./cache"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", require("./Startup/web"));
-
 
 app.use(handle);
 require("./Startup/exceptions")();
